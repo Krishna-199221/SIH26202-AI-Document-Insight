0c3f50f2716e184e81deab683c44aac13bd21413
Krishna-199221 <301186231+Krishna-199221@users.noreply.github.com>
2026-09-11 13:54:48 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173"
+        "https://sih26202-ai-document-insight-1.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
